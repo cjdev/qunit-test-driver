c5394b59d181c908f549ff6d471daac1848c4385
Darrell Hamilton <darrell.noice@gmail.com>
2013-04-02 17:35:29 -0700

    syntax cleanup

--- core/src/main/groovy/com/cj/qunitTestDriver/QUnitElement.groovy
+++ core/src/main/groovy/com/cj/qunitTestDriver/QUnitElement.groovy
@@ -1,26 +1,23 @@
 package com.cj.qunitTestDriver
 
-import static org.junit.Assert.*
+import static org.junit.Assert.*;
 
-import com.gargoylesoftware.htmlunit.html.DomNode
-import com.gargoylesoftware.htmlunit.html.HtmlElement
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 
 class QUnitElement {
-    private HtmlElement element
-    private final PageDriver driver
+    private HtmlElement element;
+    private final PageDriver driver;
 
     QUnitElement(PageDriver driver) {
-        this.driver = driver
+        this.driver = driver;
     }
-	
-	HtmlElement withClass(String className) {
-		return (HtmlElement) getElementByXPath("//*[contains(@class, '" + className + "')]").get(0);
-	}
-	
-	private List<DomNode> getElementByXPath(String xPath){
-		return (List<DomNode>) driver.page.getByXPath(xPath)
-	}	
-	
 
-	
+    HtmlElement withClass(String className) {
+        return (HtmlElement) getElementByXPath("//*[contains(@class, '" + className + "')]").get(0);
+    }
+
+    private List<DomNode> getElementByXPath(String xPath){
+        return (List<DomNode>) driver.page.getByXPath(xPath);
+    }	
 }
