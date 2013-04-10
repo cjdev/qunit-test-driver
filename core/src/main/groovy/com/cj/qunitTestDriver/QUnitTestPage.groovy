1f48aa098c03a73e798d3d616096d67b230204dd
David Ron <david.m.ron@gmail.com>
2013-04-10 16:48:22 -0700

    Automatic retry of NPE to work around an HTMLUnit bug.

--- core/src/main/groovy/com/cj/qunitTestDriver/QUnitTestPage.groovy
+++ core/src/main/groovy/com/cj/qunitTestDriver/QUnitTestPage.groovy
@@ -7,24 +7,26 @@ import com.gargoylesoftware.htmlunit.html.DomNode;
 
 
 public class QUnitTestPage {
-    private static final String TESTS_COMPLETED_STRING = "Tests completed in";
-    private static final String GLOBAL_FAILURE_STRING = "global failure";
-    public PageDriver driver;
+    private static final String TESTS_COMPLETED_STRING = "Tests completed in"
+    private static final String GLOBAL_FAILURE_STRING = "global failure"
+    public PageDriver driver
+	Integer timeout
     
 
     public QUnitTestPage(URL url, timeout, BrowserVersion browserVersion, Boolean waitForTestsToFinish) {
         driver = new PageDriver(url.toString(), browserVersion);
-        driver.waitForAjax();
+        driver.waitForAjax()
+		this.timeout = timeout
 
         if(waitForTestsToFinish)
-            waitForQunitTests(timeout);
+            waitForQunitTests()
     }
 
     public QUnitTestPage(int localPort, String relativePathOfTest, Integer testTimeout, BrowserVersion browserVersion, Boolean waitForTestsToFinish) {
         this("http://localhost:$localPort/$relativePathOfTest".toURL(), testTimeout, browserVersion, waitForTestsToFinish);
     }
 
-    void waitForQunitTests(Integer timeout) {
+    void waitForQunitTests() {
         driver.waitForTextToBePresent(
                 [TESTS_COMPLETED_STRING, GLOBAL_FAILURE_STRING],
                 timeout
@@ -34,34 +36,34 @@ public class QUnitTestPage {
 
     public Status status(){
         boolean isComplete = driver.containsText(TESTS_COMPLETED_STRING) || driver.containsText(GLOBAL_FAILURE_STRING);
-        int numPassed = passed();
-        int numFailed = failed();
-        int numFailedAssertions = 0;
+        int numPassed = passed()
+        int numFailed = failed()
+        int numFailedAssertions = 0
         String brokenTests = "";
         List<TestStatus> tests = [];
 
         if(numPassed > 0 || numFailed > 0) {
-            List<DomNode> testNodes = driver.findElementsByXPath("//ol[@id='qunit-tests']//li[contains(@id,'test-output')]");
+            List<DomNode> testNodes = driver.findElementsByXPath("//ol[@id='qunit-tests']//li[contains(@id,'test-output')]")
 
             def modules = [:]
 
                 for(DomNode testNode : testNodes) {
-                    DomNode moduleDomNode = testNode.getFirstByXPath(".//*[@class='module-name']");
-                    String moduleName = moduleDomNode != null ? moduleDomNode.asText() : "<unnamed module>";
+                    DomNode moduleDomNode = testNode.getFirstByXPath(".//*[@class='module-name']")
+                    String moduleName = moduleDomNode != null ? moduleDomNode.asText() : "<unnamed module>"
 
-                    DomNode testNameDomNode = testNode.getFirstByXPath(".//*[@class='test-name']");
-                    String testName = testNameDomNode.asText();
+                    DomNode testNameDomNode = testNode.getFirstByXPath(".//*[@class='test-name']")
+                    String testName = testNameDomNode.asText()
 
 
                     TestStatus test = new TestStatus(testName, moduleName, testNode) 
-                        tests.add(test);
+                    tests.add(test)
 
                     brokenTests += "\nTest Name: ${moduleName}: ${testName}"
 
-                        test.failures.each { failure ->
-                            numFailedAssertions++
-                                brokenTests += "\n\tFailed Assertion: ${failure}"
-                        }
+                    test.failures.each { failure ->
+                        numFailedAssertions++
+                            brokenTests += "\n\tFailed Assertion: ${failure}"
+                    }
                 }
         }
 
@@ -88,7 +90,7 @@ public class QUnitTestPage {
             );
         }
 
-        Status status = status();
+        Status status = status()
 
         if(status.numFailed > 0) {
             throw new AssertionError("${status.numFailedAssertions} assertions failed. ${status.brokenTests}\n\n\n${driver.getPage().asText()}")
